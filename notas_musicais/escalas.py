b9f17df47177e6ac381e1cb2b67be2f399473848
Mth-Amorim <65774161+Mth-Amorim@users.noreply.github.com>
2023-04-05 21:33:53 -0300

    Adicionando documentacao

---
+++ notas_musicais/escalas.py
@@ -0,0 +1,29 @@
+NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
+
+
+def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+    """
+    Gera uma escala partindo de uma tônica e uma tonalidade 
+
+    Parameters: 
+        tonica: nota que será a tônica da escala 
+        tonalidade: tonalidade da escala 
+    Returns:
+        um dicionario com as notas da escala e os graus 
+    Examples: 
+        >>> escalas('C','maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+
+        >>> escalas('A','maior')
+        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    """
+    intervalos = ESCALAS[tonalidade]
+    tonica_posicao = NOTAS.index(tonica)
+
+    temp = []
+
+    for intervalo in intervalos:
+        nota = (tonica_posicao + intervalo) % 12
+        temp.append(NOTAS[nota])
+    return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
