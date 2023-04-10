a65de0322efe98ddb48acaa5c4c66624a12c8486
Mth-Amorim <65774161+Mth-Amorim@users.noreply.github.com>
2023-04-10 00:23:55 -0300

    Implementação de teste de escalas

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -2,28 +2,45 @@ NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
 ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
-def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
-    Gera uma escala partindo de uma tônica e uma tonalidade 
+    Gera uma escala partindo de uma tônica e uma tonalidade.
 
-    Parameters: 
-        tonica: nota que será a tônica da escala 
-        tonalidade: tonalidade da escala 
+    Parameters:
+        tonica: Nota que será a tônica da escala.
+        tonalidade: Tonialidade da escala.
+
     Returns:
-        um dicionario com as notas da escala e os graus 
-    Examples: 
-        >>> escalas('C','maior')
+        um dicionario com as notas da escala e os graus.
+
+    Raises:
+        ValueErros: Caso a tônica não seja valida.
+        KeyError: Caso a escala não exista ou não tenha sido implementada.
+
+    Examples:
+        >>> escala('C','maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
-        >>> escalas('A','maior')
+        >>> escala('a','maior')
         {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
-    intervalos = ESCALAS[tonalidade]
-    tonica_posicao = NOTAS.index(tonica)
+    tonica = tonica.upper()
+    try:
+        tonica_posicao = NOTAS.index(tonica)
+        intervalos = ESCALAS[tonalidade]
+
+    except ValueError:
+        raise ValueError(f'Essa nota não existe, tente uma dessas {NOTAS}')
+
+    except KeyError:
+        raise KeyError(
+            f'Essa escala não existe ou não foi implementada, tente uma dessas {list(ESCALAS.keys())}'
+        )
 
     temp = []
 
     for intervalo in intervalos:
         nota = (tonica_posicao + intervalo) % 12
         temp.append(NOTAS[nota])
+
     return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
---
+++ tests/test_escalas.py
@@ -0,0 +1,61 @@
+from pytest import mark, raises
+
+from notas_musicais.escalas import ESCALAS, NOTAS, escala
+
+
+def test_deve_funcionar_com_chars_minusculas():
+    # Arrumrar
+    tonica = 'c'
+    tonalidade = 'maior'
+
+    # act - chamar o testar
+    result = escala(tonica, tonalidade)
+
+    # assert
+    assert result
+
+
+def teste_deve_retornar_uma_erro_quando_a_nota_nao_existe():
+    tonica = 'X'
+    tonalidade = 'maior'
+
+    mensagem_de_erro = f'Essa nota não existe, tente uma dessas {NOTAS}'
+
+    with raises(ValueError) as error:
+        escala(tonica, tonalidade)
+
+    assert mensagem_de_erro == error.value.args[0]
+
+
+def teste_deve_retornar_um_erro_quado_uma_escala_nao_existe():
+    tonica = 'c'
+    tonalidade = 'tonalidade'
+
+    mensagem_de_erro = f'Essa escala não existe ou não foi implementada, tente uma dessas {list(ESCALAS.keys())}'
+
+    with raises(KeyError) as error:
+        escala(tonica, tonalidade)
+
+    assert mensagem_de_erro == error.value.args[0]
+
+
+@mark.parametrize(
+    'tonica,esperado',
+    [
+        ('C', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
+        ('C#', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
+        ('F', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+    ],
+)
+def teste_deve_retornar_as_notas_corretas(tonica, esperado):
+    resultado = escala(tonica, 'maior')
+    assert resultado['notas'] == esperado
+
+
+def test_deve_retornar_os_ete_graus():
+    tonica = 'c'
+    tonalidade = 'maior'
+    esperado = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+    resultado = escala(tonica, tonalidade)
+
+    assert resultado['graus'] == esperado
