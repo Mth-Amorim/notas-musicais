5768a16d643d45d142de4eb438d609b76b38c70a
Matheus <mth_p_amorim+git@hotmail.com>
2025-01-06 22:15:18 -0300

    Docs sobre tutrias contribua e ajuste na doc da escala

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -1,19 +1,53 @@
+"""
+Módulo das escalas musicais.
+
+Attributes:
+   ESCALAS: Escalas implementadas usando a notação de inteiros
+   NOTAS: Notas musicais
+
+# ESCALAS
+
+As escalas estão implementadas em uma constante chamada `ESCALAS`. Que é um dicionário onde as chaves são os nomes das escalas. Se quiser ver todas as escalas implementadas pode usar:
+
+```py title="No seu shell interativo"
+>>> from notas_musicais.escalas import ESCALAS
+>>> ESCALAS
+{'maior': (0, 2, 4, 5, 7, 9, 11), 'menor': (0, 2, 3, 5, 7, 8, 10)...}
+
+```
+
+A notação inteira para as escalas foi retirada da página [List of musical scales and modes](https://en.wikipedia.org/wiki/List_of_musical_scales_and_modes) na wikipedia.
+
+tip: Dica!
+    Você pode contribuir com novas escalas usando a notação inteira:
+    [Escalas wikipedia](https://en.wikipedia.org/wiki/List_of_musical_scales_and_modes).
+    Todos os Pull Requests serão bem vindos! :heart:
+
+# NOTAS
+
+As notas estão sendo definidas em uma constante `NOTAS`. Foi optado por menter somente as notas no formato Natural e o Sustenido (#) para a simplificação do fluxo de trabalho. Embora não esteja totalmente correto. Para ver as 12 notas você pode:
+
+```py title="No seu shell interativo"
+>>> from notas_musicais.escalas import NOTAS
+>>> NOTAS
+['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+```
+"""
 NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
-ESCALAS = {'maior': [0, 2, 4, 5, 7, 9, 11], 'menor': [0, 2, 3, 5, 7, 8, 10]}
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11), 'menor': (0, 2, 3, 5, 7, 8, 10)}
 
 
-def escala(tonica, tonalidade):
+def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
-    Gera uma escala musical baseada na tônica e tonalidade fornecidas.
+    Gera uma escala apartir de uma tônica e uma tonalidade.
 
     Args:
-        tonica (str): A nota inicial da escala (ex: 'C', 'A', 'G#').
-        tonalidade (str): A tonalidade da escala (ex: 'maior', 'menor').
+        tonica: Nota que será a tônica da escala
+        tonalidade: Tonialidade da escala
 
     Returns:
-        dict: Um dicionário contendo as notas da escala e seus respectivos graus.
-            - 'notas': Lista de notas na escala.
-            - 'graus': Lista de graus da escala.
+        Um dicionário com as notas da escala e os graus.
 
     Raises:
         ValueError: Caso a tônica não seja uma nota valida.
@@ -22,9 +56,6 @@ def escala(tonica, tonalidade):
     Examples:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
-
-        >>> escala('a', 'maior')
-        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
         >>> escala('a', 'menor')
         {'notas': ['A', 'B', 'C', 'D', 'E', 'F', 'G'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
@@ -36,7 +67,7 @@ def escala(tonica, tonalidade):
     except ValueError:
         raise ValueError(f'Essa nota não existe, tente uma dessas {NOTAS}')
     except KeyError:
-        raise KeyError(f'Essa escala não existe ou não foi implementada. Tente uma dessas {list(ESCALAS.keys())}')
+        raise KeyError('Essa escala não existe ou não foi implementada. ' f'Tente uma dessas {list(ESCALAS.keys())}')
 
     temp = []
 
